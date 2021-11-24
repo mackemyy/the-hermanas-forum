293bf7d330376c54ecdb2f4cfc58bcae76c032fa
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2021-11-24 16:48:38 +0800

    Added User Login Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserWelcomeComponent } from './user-welcome/user-welcome.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserWelcomeComponent
+    UserWelcomeComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
