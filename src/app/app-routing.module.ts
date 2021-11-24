69231b46a1b4e6bdc47edb4afcb1d19a8e6bc8b4
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2021-11-24 18:09:02 +0800

    Updated Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLoginComponent } from './user-login/user-login.component';
 import { UserWelcomeComponent } from './user-welcome/user-welcome.component';
 
 const routes: Routes = [
   {
     path: '', component: UserWelcomeComponent,
+  },
+  {
+    path: 'user-login', component: UserLoginComponent,
   },
   
 ];
