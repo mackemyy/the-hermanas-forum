2d289519f6eb7f1f4e79c836fa4d182ebe2f106f
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2021-11-24 16:34:38 +0800

    Added Welcome Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserWelcomeComponent } from './user-welcome/user-welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: UserWelcomeComponent,
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
