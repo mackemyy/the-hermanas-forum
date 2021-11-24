f0cdf9a00f0edbfea2167a63a647969378fb4272
Ruby Mae Mutya <ruby.mutya@usjr.edu.ph>
2021-11-24 17:33:29 +0800

    Update Admin Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,11 +17,13 @@ import { AdminLoginComponent } from './admin-login/admin-login.component';
     UserRegisterComponent,
     UserLoginComponent,
     AdminLoginComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
