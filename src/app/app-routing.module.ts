d97fb618975cc8866c5f980f7741ffdf573d0fb9
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2021-11-24 17:00:28 +0800

    Updated router links on buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
 import { UserWelcomeComponent } from './user-welcome/user-welcome.component';
 
 const routes: Routes = [
   {
     path: '', component: UserWelcomeComponent,
+  },
+  {
+    path: 'user-login', component: UserLoginComponent,
+  },
+  {
+    path: 'user-register', component: UserRegisterComponent,
+  },
+  {
+    path: 'admin-login', component: AdminLoginComponent,
   },
   
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserWelcomeComponent } from './user-welcome/user-welcome.component';
 import { UserRegisterComponent } from './user-register/user-register.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserWelcomeComponent,
-    UserRegisterComponent
+    UserRegisterComponent,
+    UserLoginComponent,
+    AdminLoginComponent,
   ],
   imports: [
     BrowserModule,
